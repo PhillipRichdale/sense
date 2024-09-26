b2c59ce05a8167f2568dcaa02cab53ef6b6ff44e
Phillip Richdale <office@richdale.de>
2024-09-26 11:31:21 +0200

    wip

--- resources/js/Pages/Map.vue
+++ resources/js/Pages/Map.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import Tunnel from "@/Symbols/Tunnel.vue";
 import Factory from "@/Symbols/Factory.vue";
+import Tile from "@/Symbols/Tile.vue";
 </script>
 
 <template>
@@ -15,73 +16,73 @@ import Factory from "@/Symbols/Factory.vue";
 			<div class="sensemap">
 				<div class="rows shift">
 					<div class="tile invisible"></div>
-					<div class="tile">A0</div>
+					<Tile id="a0"/>
 					<div class="tile invisible"></div>
 					<div class="tile invisible"></div>
-					<div class="tile">D0</div>
+					<Tile id="d0"/>
 					<div class="tile invisible"></div>
 					<div class="tile invisible"></div>
 					<div class="tile invisible"></div>
 				</div>
 				<div class="rows">
 					<div class="tile invisible"></div>
-					<div class="tile">A1</div>
-					<div class="tile">B1</div>
-					<div class="tile">C1</div>
-					<div class="tile">D1</div>
-					<div class="tile">E1</div>
-					<div class="tile">F1</div>
+					<Tile id="a1"/>
+					<Tile id="b1"/>
+					<Tile id="c1"/>
+					<Tile id="d1"/>
+					<Tile id="e1"/>
+					<Tile id="f1"/>
 					<div class="tile invisible"></div>
 				</div>
 
 				<div class="rows shift">
-					<div class="tile">A2</div>
-					<div class="tile">B2</div>
-					<div class="tile">C2</div>
-					<div class="tile">D2
+					<Tile id="a2"/>
+					<Tile id="b2"/>
+					<Tile id="c2"/>
+					<Tile id="d2">
 						<Tunnel/>
-					</div>
-					<div class="tile">E2</div>
-					<div class="tile">F2</div>
-					<div class="tile">G2</div>
+					</Tile>
+					<Tile id="e2"/>
+					<Tile id="f2"/>
+					<Tile id="g2"/>
 				</div>
 				<div class="rows">
-					<div class="tile">A3</div>
-					<div class="tile">B3</div>
-					<div class="tile">C3
+					<Tile id="a3"/>
+					<Tile id="b4"/>
+					<Tile id="c3">
 						<Tunnel/>
-					</div>
-					<div class="tile">D3</div>
-					<div class="tile">E3</div>
-					<div class="tile">F3
+					</Tile>
+					<Tile id="d3"/>
+					<Tile id="e3"/>
+					<Tile id="f3">
 						<Tunnel/>
-					</div>
-					<div class="tile">G3</div>
-					<div class="tile">H3</div>
+					</Tile>
+					<div id="tile-g3" class="tile">G3</div>
+					<div id="tile-h3" class="tile">H3</div>
 				</div>
 				<div class="rows shift">
-					<div class="tile">B4</div>
-					<div class="tile">C4</div>
-					<div class="tile">D4</div>
-					<div class="tile">E4
+					<div id="tile-b4" class="tile">B4</div>
+					<div id="tile-c4" class="tile">C4</div>
+					<div id="tile-d4" class="tile">D4</div>
+					<div id="tile-e4" class="tile">E4
 						<Factory />
 					</div>
-					<div class="tile">F4</div>
-					<div class="tile">G4</div>
-					<div class="tile">H4</div>
+					<div id="tile-f4" class="tile">F4</div>
+					<div id="tile-g4" class="tile">G4</div>
+					<div id="tile-h4" class="tile">H4</div>
 				</div>
 				<div class="rows">
-					<div class="tile">B5</div>
-					<div class="tile">C5</div>
-					<div class="tile">D5
+					<div id="tile-b5" class="tile">B5</div>
+					<div id="tile-c5" class="tile">C5</div>
+					<div id="tile-d5" class="tile">D5
 						<Tunnel/>
 					</div>
-					<div class="tile">E5</div>
-					<div class="tile">F5</div>
-					<div class="tile">G5
+					<div id="tile-e5" class="tile">E5</div>
+					<div id="tile-f5" class="tile">F5</div>
+					<div id="tile-g5" class="tile">G5
 						<Tunnel/>
 					</div>
-					<div class="tile">H5</div>
+					<div id="tile-h5" class="tile">H5</div>
 				</div>
 				<div class="rows shift">
 					<div class="tile">C6</div>
---
+++ resources/js/Symbols/Tile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(['id'])
+</script>
+<template>
+	<div id="tile-{{id}}" class="tile">
+		<span class="tile-id">{{id}}</span>
+		<slot/>
+	</div>
+</template>
